7e7e953875dcb737e259ad197b8e0534e1828db9
nicholashui8 <nicholashui8@gmail.com>
2020-08-27 12:50:51 -0700

    added ascii art

--- src/HW1.java
+++ src/HW1.java
@@ -1,6 +1,16 @@
 public class HW1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        
+        System.out.println("            __     __\n" +
+                "           /  \\~~~/  \\\n" +
+                "     ,----(     ..    )\n" +
+                "    /      \\__     __/\n" +
+                "   /|         (\\  |(\n" +
+                "  ^ \\   /___\\  /\\ |   hjw\n" +
+                "     |__|   |__|-\"    `97\n" +
+                "\n" +
+                "------------------------------------------------\n" +
+                "Thank you for visiting https://asciiart.website/\n" +
+                "This ASCII pic can be found at\n" +
+                "https://asciiart.website//index.php?art=animals/elephants\n");
     }
 }
